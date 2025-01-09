ad7a38abe993f1724bae3219deaf9c4c2d9082dd
Cédric, Olivier CHAUVET <chauvetc.ujf@gmail.com>
2025-01-09 12:51:06 +0100

    Add files via upload

---
+++ marcheur/glissemenent_jambe_gauche/glissemenent_jambe_gauche.ino
@@ -0,0 +1,124 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <math.h>
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// Depending on your servo make, the pulse width min and max may vary
+// il faudrait encore modifier ces deux variable de maniere plus limitante  pour le
+// mouvement mais qui protè
+#define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
+
+// points milieux de chaques  servomoteurs
+int p1 = 94;
+int p2 = 93;
+int p3 = 90;
+int p4 = 95;
+
+// dimensions du  robot
+float h  = 18.5; // hauteur du robot
+float l = 11.0; // longeur tibia considerant que les deux parties de la jambe aient meme longueur
+
+const float pi = 3.14159267;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("16 channel Servo test!");
+
+  pwm.begin();
+  Serial.println("angletopulse depart:");
+  Serial.println(angleToPulse(p3+43.5));
+    Serial.println("angletopulseArrivée:");
+  Serial.println(angleToPulse(p3 +12.5));
+ 
+ 
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+}
+
+// calculée par l'exercice 3 de la documentation
+// elle demontre que 2 jambes assymetriques, peuvent etre pilotées 
+// tout en gardant une hauteur constante, voir calcul...
+
+void loop() {
+  pwm.setPWM(2, 0, angleToPulse(p2+30)) ;      
+  pwm.setPWM(4, 0, angleToPulse(p4-30)); 
+   
+  for(int p = 380; p< 457; p++){
+    float A = pulseToAngle(p);
+    float B = acos( h/l - cos(A));
+    // Serial.println(A);
+    Serial.println(B);
+    B = 180.0 / pi *B; 
+   
+    int pulseB = angleToPulse(B);
+    pwm.setPWM(3, 0, p);
+    //pwm.setPWM(1, 0, pulseB);
+    delay(200);
+  }
+
+  for(int p = 457; p> 380; p--){
+    float A = pulseToAngle(p);
+    float B = acos( h/l - cos(A * pi /180.0));
+    Serial.println(A);
+    //Serial.println(B);
+    
+    B = 180 / pi *B; 
+    int pulseB = angleToPulse(B);
+    pwm.setPWM(3, 0, p);
+    //pwm.setPWM(1, 0, pulseB);
+    delay(200);  
+    }
+}
+
+
+int angleToPulse(int angle){
+   
+   // mapping de 0 a 180
+   //int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);
+   
+   // mapping de 20 a 160 degres, avec constrain pour protection des moteurs
+   angle = constrain(angle, 20, 160);
+   int pulse = map(angle,20, 160, 175,525);
+   
+//   Serial.print("atp_Angle: ");Serial.print(angle);
+//   Serial.print("atp_pulse: ");Serial.println(pulse);
+   return pulse;
+}
+
+int pulseToAngle(int pulse){
+   
+   // mapping de 0 a 180
+   //int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);
+   
+   // mapping de 20 a 160 degres, avec constrain pour protection des moteurs
+   pulse = constrain(pulse, 175, 525);
+   int angle = map(pulse, 175, 525, 20, 160);
+//   
+//   Serial.print("pta_Angle: ");Serial.print(angle);
+//   Serial.print("pta_pulse: ");Serial.println(pulse);
+   return angle;
+}
+
+//
+//float degrees(float radians) {
+//  return radians / 2 / pi * 360;
+//}
+//float radians(float degrees) {
+//  return degrees / 360 * 2 * pi;
+//}
+/*
+   // jambe gauche avancé
+   pwm.setPWM(1, 0, angleToPulse(p1-12.5));
+C   pwm.setPWM(3, 0, angleToPulse(p3+43.5));
+   pwm.setPWM(4, 0, angleToPulse(p4-30)); 
+  *
+  *
+  *
+   // jambe gauche reculée
+   pwm.setPWM(1, 0, angleToPulse(p1-43));
+   pwm.setPWM(2, 0, angleToPulse(p2+30)) ;      
+   pwm.setPWM(3, 0, angleToPulse(p3+12.5));
+   pwm.setPWM(4, 0, angleToPulse(p4-30)); 
+   
+ */
